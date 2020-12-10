76c5e2689608d0ff395f5b59fe8fcdf4067743c3
vishwas-katiyar <vishwaskatiyar@users.noreply.github.com>
2020-12-11 01:06:47 +0530

    last

--- Tweetproject/wsgi.py
+++ Tweetproject/wsgi.py
@@ -1,17 +1,7 @@
 import os
-import sys
 
-# ADD YOUR PROJECT TO THE PYTHONPATH FOR THE PYTHON INSTANCE
-path = '\.'
+from django.core.wsgi import get_wsgi_application
 
-if path not in sys.path:
-    sys.path.append(path)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tweetproject.settings')
 
-os.chdir(path)
-
-# TELL DJANGO WHERE YOUR SETTINGS MODULE IS LOCATED
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Tweetproject.settings'
-
-# IMPORT THE DJANGO WSGI HANDLER TO TAKE CARE OF REQUESTS
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
